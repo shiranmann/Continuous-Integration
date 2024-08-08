6fb9eb3ba0355447a0ae91ce256bea1e7279bc00
shiranmann <162421421+shiranmann@users.noreply.github.com>
2024-08-08 07:11:02 +0000

    input ip

--- app/index.js
+++ app/index.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const app = express()
 const port = 9001
+const ip = require('ip')
 
 app.get('/', (req, res) => {
-  res.send('Hello World! from kubernetes')
+  res.send(`Hello World! from kubernetes: ${ip.address}`)
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${ip.address} ${port}`)
 })
